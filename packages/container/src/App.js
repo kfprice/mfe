1fe001bc0fe7ee8246ef3c5ccb493906210ecb48
kemet price <Kemet.Price1@gmail.com>
2023-02-09 13:17:28 -0500

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import MarketingApp from "./components/MarketingApp";
 import Header from './components/Header';
 
-// Testing new branch
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
+
 export default () => {
     return (
-<BrowserRouter> 
+<BrowserRouter>
+  <StylesProvider generateClassName={generateClassName}>
     <div>
         <Header />
         <MarketingApp />
     </div>
-    </BrowserRouter>
+  </StylesProvider>
+</BrowserRouter>
     );
 };
